f35c984cf27915b192c24a2cf72aa1dd83c6ebf1
lecturnotes <44796054+lecturnotes@users.noreply.github.com>
2024-10-19 16:58:25 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+//var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log(a)
+    //console.log(b)
+
+}
+
+for(let i=0; i<array.length; $i++){
+    const element = array[i]
+}
+
+console.log(a)//10 inside scope-undefined
+//console.log(b)//20 inside scope-undefined
+//console.log(c)//30 inside scope-30
